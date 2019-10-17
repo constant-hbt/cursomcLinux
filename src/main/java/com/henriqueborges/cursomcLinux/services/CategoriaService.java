2507113283bd145bfc41192988229a76e2952c71
Henrique Borges <henrique17_votu@hotmail.com>
2019-10-17 16:21:58 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/henriqueborges/cursomcLinux/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.henriqueborges.cursomcLinux.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.henriqueborges.cursomcLinux.domain.Categoria;
+import com.henriqueborges.cursomcLinux.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
