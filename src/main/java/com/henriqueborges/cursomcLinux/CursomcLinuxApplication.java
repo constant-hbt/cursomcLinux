1974cde4629d942f2a26c5cfbc2f9c59b862e2e0
Henrique Borges <henrique17_votu@hotmail.com>
2019-10-17 13:57:53 -0300

    Projeto criado

---
+++ src/main/java/com/henriqueborges/cursomcLinux/CursomcLinuxApplication.java
@@ -0,0 +1,13 @@
+package com.henriqueborges.cursomcLinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcLinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcLinuxApplication.class, args);
+	}
+
+}
