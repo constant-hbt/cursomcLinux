f9293b2c7785f524a92ce7fb5159d7286a2669ad
Henrique Borges <henrique17_votu@hotmail.com>
2019-10-22 15:49:52 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/henriqueborges/cursomcLinux/dto/CategoriaDTO.java
@@ -0,0 +1,40 @@
+package com.henriqueborges.cursomcLinux.dto;
+
+import java.io.Serializable;
+
+import com.henriqueborges.cursomcLinux.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+		id = 0;
+		nome = new String();
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
--- src/main/java/com/henriqueborges/cursomcLinux/resources/CategoriaResource.java
+++ src/main/java/com/henriqueborges/cursomcLinux/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.henriqueborges.cursomcLinux.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.henriqueborges.cursomcLinux.domain.Categoria;
+import com.henriqueborges.cursomcLinux.dto.CategoriaDTO;
 import com.henriqueborges.cursomcLinux.services.CategoriaService;
 
 @RestController
@@ -46,5 +49,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete (@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listado = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listado);
 	}
 }
