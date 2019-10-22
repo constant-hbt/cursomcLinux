847e78a6d6b5be1d438cf9835e74a02235590da9
Henrique Borges <henrique17_votu@hotmail.com>
2019-10-22 16:40:29 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/henriqueborges/cursomcLinux/services/CategoriaService.java
+++ src/main/java/com/henriqueborges/cursomcLinux/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.henriqueborges.cursomcLinux.domain.Categoria;
+import com.henriqueborges.cursomcLinux.dto.CategoriaDTO;
 import com.henriqueborges.cursomcLinux.repositories.CategoriaRepository;
 import com.henriqueborges.cursomcLinux.services.exceptions.DataIntegrityException;
 import com.henriqueborges.cursomcLinux.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
